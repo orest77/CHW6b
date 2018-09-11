8c9088e9899309aaf9447576103d1c7f525ae8d9
orest77 <orest.shkhumov@gmail.com>
2018-09-11 14:06:08 +0300

    first commit

--- HW6b/Program.cs
+++ HW6b/Program.cs
@@ -10,6 +10,42 @@ namespace HW6b
     {
         static void Main(string[] args)
         {
+
+            int numbers;
+            int start = 0;
+            int end = 100;
+            try
+            {
+                Console.WriteLine("Enter 10 numbers: \n");
+                //Numbers num = new Numbers();
+                for (int i = 0; i < 10; i++)
+                {
+                    Console.WriteLine("Input  numbers:{0} ", i + 1);
+                    numbers = ReadNumber(start, end);
+
+                }
+            }
+            catch (Exception e)
+            {
+                Console.Error.WriteLine("Wrong{0}:::::{1}", e.GetType(), e.Message);
+            }
+            Console.ReadKey();
+
+        }
+
+        static int ReadNumber(int start, int end)
+        {
+
+            int number = Convert.ToInt32(Console.ReadLine());
+            if (number > start && number <= end)
+            {
+                return number;
+            }
+            else
+            {
+                throw new OverflowException("Number not in the range 1 to 100");
+            }
+
         }
     }
 }
